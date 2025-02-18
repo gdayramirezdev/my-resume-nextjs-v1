a40353dd5840518fc8af921fab1beb47242c8d38
JOSE CRUZ RAMIREZ <jose@MacBook-Pro-de-JOSE.local>
2025-02-17 23:37:03 -0600

    set classnames into progress bar

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -10,11 +10,21 @@ type SkillsProps = {
 export function ProgressBar(props: Skill) {
   const { name, average} = props;
 
+  const classnames: any = {
+    0: '',
+    25: 'w-1/4',
+    50: 'w-1/2',
+    75: 'w-3/4',
+    100: 'w-full'
+  }
+
+  const classname: string = classnames[average];
+
   return (
     <div className="flex flex-col items-start space-y-2">
       <span className="text-sm text-gray-600">{name}</span>
       <div className="w-full bg-gray-200 h-1 ">
-        {average && <div className={`bg-blue-600 h-1 w-[${average}%]`} />}
+        <div className={`bg-blue-600 h-1 ${classname}`} />
       </div>
     </div>
   );
